33ba4dcbccc7cd0909d0f694cfe265e4be3551af
williams chorolque <wchorolque@gmail.com>
2023-11-07 04:23:54 -0400

    Agregado configuración de EntityFramework

---
+++ src/Models/TareasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace src.Models
+{
+    public class TareasContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        public TareasContext(DbContextOptions<TareasContext> options)
+            :base(options)
+        {
+        }
+    }
+}
