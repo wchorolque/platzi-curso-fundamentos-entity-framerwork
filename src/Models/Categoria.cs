58621326af34bc2fd9b1613dd9bb6b442f89df2d
williams chorolque <wchorolque@gmail.com>
2023-11-07 04:32:07 -0400

    Agregado atributos mediante DataAnnotations

--- src/Models/Categoria.cs
+++ src/Models/Categoria.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace src.Models;
 
 public class Categoria {
 
+    [Key]
     public Guid CategoriaId { get; private set; }
 
+    [Required]
+    [MaxLength(150)]
     public String Nombre { get; private set; }
 
     public String Descripcion { get; private set; }
