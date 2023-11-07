f5a421857647cf14c3bcee97e44872fc8952b711
williams chorolque <wchorolque@gmail.com>
2023-11-07 04:43:16 -0400

    Creado base de datos en memoria y probado con postman

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using src;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(options => options.UseInMemoryDatabase("TareasDB"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();
+
+    return Results.Ok("Base de datos en memoria: " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
